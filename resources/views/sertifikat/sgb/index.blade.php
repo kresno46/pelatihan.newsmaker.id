bd14392cd5d74a52507c0fd4c58d31e7f9c4e8ae
Kresno Nugroho <kresnonugroho46@gmail.com>
2025-09-16 16:30:18 +0700

    update:sertif sgb

--- resources/views/sertifikat/sgb/index.blade.php
+++ resources/views/sertifikat/sgb/index.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="id">
 
 <head>
@@ -22,7 +22,7 @@
             width: 100%;
             height: 100vh;
             border-collapse: collapse;
-            /* margin-left: 20px; */
+            margin-left: 20px;
         }
 
         td {
@@ -123,6 +123,121 @@
             </td>
         </tr>
     </table>
+
+</body>
+
+</html> --}}
+
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <style>
+        @page {
+            size: A4 landscape;
+            margin: 0;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Times New Roman', Times, serif;
+            background-image: url('{{ public_path('sertifikat-stamp/SGB/SGB.png') }}');
+            background-size: cover;
+            background-position: center;
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+        }
+
+        .content {
+            text-align: center;
+            padding-top: 180px; /* dikurangi biar muat */
+        }
+
+        .content .header {
+            font-size: 20px;
+            margin-bottom: 10px;
+        }
+
+        .name {
+            font-size: 34px;
+            font-weight: bold;
+            border-bottom: 1px solid #000;
+            display: inline-block;
+            padding: 0 30px;
+            margin: 10px 0;
+        }
+
+        .description {
+            font-size: 20px;
+        }
+
+        .level-title {
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        .date-sign {
+            font-size: 14px;
+            text-align: center;
+            padding-bottom: 30px; /* biar ga nabrak bawah */
+        }
+
+        .date {
+            font-size: 18px;
+        }
+
+        .stamp {
+            width: 100px;
+            margin: 10px 0;
+        }
+
+        .ttd {
+            font-size: 18px;
+            font-weight: bold;
+            margin-top: 10px;
+        }
+
+        .title {
+            font-size: 16px;
+            margin-top: 5px;
+        }
+
+        .cert-id {
+            font-size: 12px;
+            color: #555;
+            text-align: center;
+            margin-top: 10px;
+        }
+
+        .signature-line {
+            width: 200px;
+            border: 1px solid #000;
+            margin: 5px auto;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="content">
+        <p class="header">DIBERIKAN KEPADA:</p>
+        <p class="name">{{ $name }}</p>
+        <p class="description">Telah mengikuti:</p>
+        <p class="level-title">{{ $levelTitle }}</p>
+    </div>
+
+    <div class="date-sign">
+        <p class="date">Jakarta, {{ $date }}</p>
+        <img class="stamp" src="{{ public_path('sertifikat-stamp/SGB/STAMP-SGB.png') }}" alt="Stamp">
+        <p class="ttd">IRIAWAN WIDADI</p>
+        <hr class="signature-line">
+        <p class="title">Direktur Utama</p>
+        <div class="cert-id">ID Sertifikat: {{ $uuid }}</div>
+    </div>
 
 </body>
 
